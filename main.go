0bb51e154e0f3e19f43257889b729117c8410019
DigvijayWagh22 <waghdigvijay2010@gmail.com>
2024-10-22 07:52:23 +0530

    Added static folder and updated main.go to serve static files

--- main.go
+++ main.go
@@ -6,9 +6,11 @@ import (
 )
 
 func main() {
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
 	})
-
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.ListenAndServe(":80", nil)
 }
